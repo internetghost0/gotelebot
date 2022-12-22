45a46ccb0c627fcd1a61f952081f223d5b873b1d
zer0 <internetghost0@protonmail.com>
2022-12-22 16:18:14 +0000

    ready? set. go!

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-    "fmt"
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
-    fmt.Println("Hello, World!");
+	const TOKEN string = "SECRET-TOKEN"
+	bot, err := tgbotapi.NewBotAPI(TOKEN)
+	if err != nil {
+		log.Panic(err)
+	}
+	// bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil { // ignore any non-message updates
+			continue
+		}
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+		bot.Send(msg)
+	}
 }
