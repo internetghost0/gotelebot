25b5ba84a53dc8d11092fedfc3a9641131cc5235
zer0 <internetghost0@protonmail.com>
2022-12-24 13:12:54 +0000

    refactor code

--- main.go
+++ main.go
@@ -8,13 +8,28 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+// accepts command and array of arguments
+// returns the output of execution
+// example: ExecCmd("ls", [])
+func ExecCmd(cmd string, args []string) (string, error) {
+	out, err := exec.Command(cmd, args...).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+// main function
 func main() {
+    // change the token
 	const TOKEN string = "SECRET-TOKEN"
+
 	bot, err := tgbotapi.NewBotAPI(TOKEN)
 	if err != nil {
 		log.Panic(err)
 	}
-	// bot.Debug = true
+
+    // verbose bot username
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
@@ -24,18 +39,26 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
 	for update := range updates {
-		if update.Message == nil { // ignore any non-message updates
+        // ignore any non-message updates
+		if update.Message == nil { 
 			continue
 		}
-		// log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
 		var cmds []string
 		if strings.ContainsAny(update.Message.Text, "\n") {
+            // if msg contains new-lines then split it by `\n` separator
 			cmds = strings.Split(update.Message.Text, "\n")
 		} else {
+            // otherwise split it by space separator
 			cmds = strings.Split(update.Message.Text, " ")
 		}
+
+        // verbose command and args
 		log.Printf("ExecCmd(%s, %v)", cmds[0], cmds[1:])
+
+        // Execute cmd by ExecCmd function
 		out, err := ExecCmd(cmds[0], cmds[1:])
 		if err != nil {
 			out = "[!] ERROR:\n" + err.Error()
@@ -43,14 +66,8 @@ func main() {
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, out)
 		msg.ReplyToMessageID = update.Message.MessageID
+        // send cmd-output to user
 		bot.Send(msg)
 	}
 }
 
-func ExecCmd(cmd string, args []string) (string, error) {
-	out, err := exec.Command(cmd, args...).Output()
-	if err != nil {
-		return "", err
-	}
-	return string(out), nil
-}
